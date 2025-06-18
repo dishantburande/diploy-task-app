9cf205a1eb1e3b2924efd5ee1b8d220df301672e
Dishant Burande <dishant.burande@gmail.com>
2025-06-18 16:08:14 +0530

    commit

---
+++ backend/model/taskSchema.js
@@ -0,0 +1,33 @@
+import { create, update } from "lodash";
+import mongoose from "mongoose";
+import status from "statuses";
+
+const taskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  status: {
+    type: String,
+    enum: ["completed", "incomplete"],
+    default: "incomplete",
+  },
+  archived: {
+    type: Boolean,
+    default: false,
+  },
+  createdBy: {
+    type: mongoose.Schema.ObjectId,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Task = mongoose.model("Task", taskSchema);
+
+export default Task;
